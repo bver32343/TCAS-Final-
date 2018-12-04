d6d64b5ba0dfa168107afca0ff019dab0f8a5cb2
Athiwat <bver32343@hotmail.com>
2018-12-04 15:32:15 +0700

    [update] model add username password

--- TCAS62/src/main/java/cloud/tcas/TCAS62/TCAS.java
+++ TCAS62/src/main/java/cloud/tcas/TCAS62/TCAS.java
@@ -21,6 +21,12 @@ public class TCAS {
     private long id;
    
     @NotNull
+    private String username;
+    
+    @NotNull
+    private String password;
+    
+    @NotNull
     private String firstname;
 
     @NotNull
@@ -41,6 +47,22 @@ public class TCAS {
 
     public void setId(long id) {
         this.id = id;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getFirstname() {
